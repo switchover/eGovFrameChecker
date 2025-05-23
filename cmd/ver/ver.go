a5bf365cbdd76efce55ae121329ec679481f7a77
switchover <switchover@gmail.com>
2025-05-23 09:15:39 +0000

    버전 정보 업데이트

--- cmd/ver/ver.go
+++ cmd/ver/ver.go
@@ -10,7 +10,10 @@ import "fmt"
 //   - Repository 처리 상 오류 수정
 //
 // v0.2.1 : ANTLR4 처리 오류 수정
-const CheckerVersion = "v0.2.1"
+// v0.3.0 : 
+//   - eGovFrame 기본 버전 변경 (v4.2 -> v4.3)
+//   - 확인이 필요한 중간 추상 클래스 출력
+const CheckerVersion = "v0.3.0"
 
 // build flags
 var (
