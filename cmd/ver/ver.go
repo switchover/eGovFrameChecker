d6f3d80c22b884d774806a745d054ca7524c2253
Vincent Han <switchover@gmail.com>
2025-08-10 16:55:48 +0900

    Upate version info

--- cmd/ver/ver.go
+++ cmd/ver/ver.go
@@ -8,15 +8,22 @@ import "fmt"
 //   - 상위 클래스 정보 기록 추가
 //   - 불필요 error 반환 제거
 //   - Repository 처리 상 오류 수정
+//
 // v0.2.1 : ANTLR4 처리 오류 수정
 // v0.3.0 :
 //   - eGovFrame 기본 버전 변경 (v4.2 -> v4.3)
 //   - 확인이 필요한 중간 추상 클래스 출력
+//
 // v0.3.1 : Service/Repository에서 interface 제외 처리
 // v0.4.0 :
 //   - Repository 파일 패턴 추가 (*Repository.java)
 //   - Hibernate 점검 시 상위 클래스 필드 확인 추가
-const CheckerVersion = "v0.4.0"
+//
+// v0.4.1 :
+//   - CSV Writer 오류 시 에러 조치
+//   - 설정 상 EntityManager 필드 클래스 오류 수정
+//   - Check List 처리 오류 수정 등
+const CheckerVersion = "v0.4.1"
 
 // build flags
 var (
